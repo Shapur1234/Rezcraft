aa1b4bf2f1afa916734b196d74f0a09d2b48df85
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-01-24 15:41:07 +0100

    GH actions pain

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use std::env;
 
 fn main() {
     if env::var("CARGO_CFG_TARGET_OS").is_ok_and(|target_os| target_os == "linux") {
-        // println!("cargo:rustc-link-lib=vulkan");
+        println!("cargo:rustc-link-lib=vulkan");
     }
 }
